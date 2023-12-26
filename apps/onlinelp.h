19a639371bb1f1b0ae68f11cde63655a8e81af3b
cz <chuwzhang@gmail.com>
2023-12-27 01:55:49 +0800

    add apps, works for CPU only

--- apps/onlinelp.h
+++ apps/onlinelp.h
@@ -5,13 +5,21 @@
 #ifndef CUPDLP_ONLINELP_H
 #define CUPDLP_ONLINELP_H
 
-#include <random>
-#include <unordered_set>
-#include <Eigen/Dense>
-#include <Eigen/Sparse>
-#include "../interface/mps_lp.h"
-
-
 #define DBG_ONLINE_LP (0)
 
-#endif //CUPDLP_ONLINELP_H
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <iostream>
+#include <random>
+#include <unordered_set>
+
+using namespace Eigen;
+typedef Eigen::SparseMatrix<double, Eigen::ColMajor> SpMat;
+typedef Eigen::Triplet<double> T;
+using eigen_array = ArrayXd;
+using eigen_array_int = ArrayXi;
+using eigen_buff = Eigen::Map<ArrayXd>;
+
+SpMat getRandomSpMat(size_t nRows, size_t nCols, double p);
+
+#endif  // CUPDLP_ONLINELP_H
