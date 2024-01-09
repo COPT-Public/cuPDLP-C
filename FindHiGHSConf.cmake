b1ef14fac5d30860d7801f6ea3286e284abe99d4
cz <chuwzhang@gmail.com>
2024-01-09 22:47:20 +0800

    merge highs

--- FindHiGHSConf.cmake
+++ FindHiGHSConf.cmake
@@ -1,27 +1,9 @@
 # (c) Tianhao Liu
 set(HiGHS_LIBRARY-NOTFOUND, OFF)
 message(NOTICE "Finding HiGHS environment")
-message(NOTICE "    - HiGHS Home detected at $ENV{HiGHS_HOME}")
-set(CMAKE_HiGHS_PATH "$ENV{HiGHS_HOME}")
-set(HiGHS_HEADER_LIB "$ENV{HiGHS_HOME}/build/lib")
-set(HiGHS_INCLUDE_DIR "$ENV{HiGHS_HOME}/src")
+message(NOTICE "    - HiGHS Home detected at $ENV{HIGHS_HOME}")
 
-file(GLOB_RECURSE 
-        HiGHS_HEADER_FILES 
-        "${HiGHS_INCLUDE_DIR}/*.h" 
-        "${HiGHS_INCLUDE_DIR}/*.hpp" 
-        "$ENV{HiGHS_HOME}/build/*.h"
-        "$ENV{HiGHS_HOME}/build/*.hpp"
-)
+set(HIGHS_DIR "$ENV{HIGHS_HOME}/lib/cmake/highs")
 
-find_library(HiGHS_LIBRARY
-        NAMES highs
-        PATHS "${HiGHS_HEADER_LIB}"
-        REQUIRED
-        NO_DEFAULT_PATH
-)
-
-message(NOTICE 
-        "    - HiGHS Libraries detected at ${HiGHS_LIBRARY}")
-message(NOTICE
-        "    - HiGHS include dir at ${HiGHS_INCLUDE_DIR}")
+find_package(HIGHS REQUIRED)
+find_package(Threads REQUIRED)
