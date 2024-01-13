e10b0d053157ab6ab9e47ecf741984f86a6da15e
Your Name <chuwen@shanshu.ai>
2024-01-13 20:21:50 +0800

    support highs precompiled binaries

--- FindHiGHSConf.cmake
+++ FindHiGHSConf.cmake
@@ -2,9 +2,24 @@
 set(HiGHS_LIBRARY-NOTFOUND, OFF)
 message(NOTICE "Finding HiGHS environment")
 message(NOTICE "    - HiGHS Home detected at $ENV{HIGHS_HOME}")
+set(CMAKE_HiGHS_PATH "$ENV{HIGHS_HOME}")
+set(HiGHS_HEADER_LIB "$ENV{HIGHS_HOME}/lib")
+set(HiGHS_INCLUDE_DIR "$ENV{HIGHS_HOME}/include/highs")
 
-# set(HIGHS_DIR "$ENV{HIGHS_HOME}/lib/cmake/highs")
-set(HIGHS_DIR "./highs")
+file(GLOB_RECURSE
+        HiGHS_HEADER_FILES
+        "${HiGHS_INCLUDE_DIR}/*.h"
+        "${HiGHS_INCLUDE_DIR}/*.hpp"
+)
 
-find_package(HIGHS REQUIRED)
-find_package(Threads REQUIRED)
+find_library(HiGHS_LIBRARY
+        NAMES highs
+        PATHS "${HiGHS_HEADER_LIB}"
+        REQUIRED
+        NO_DEFAULT_PATH
+)
+
+message(NOTICE
+        "    - HiGHS Libraries detected at ${HiGHS_LIBRARY}")
+message(NOTICE
+        "    - HiGHS include dir at ${HiGHS_INCLUDE_DIR}")
--- cmake/README.md
+++
@@ -1,5 +0,0 @@
-# cmake files
-
-> HiGHS
-
-The cmake files to support HiGHS are from the [HiGHS project](https://highs.dev) 
